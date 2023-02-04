3a1a821cfcce08cb175e6802ddb0855ca6cbf815
Kryxzael <kasper.ramskjell98@gmail.com>
2023-02-04 13:02:24 +0100

    Add a "tumbleweed" that can roll, decelerate and strafe

---
+++ Assets/Tumbleweeds/Scripts/CameraKeepDistance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+public class CameraKeepDistance : MonoBehaviour
+{
+    public Transform Target;
+
+    private Vector3 _distance;
+
+
+    private void Start()
+    {
+        _distance = transform.position - Target.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = Target.position + _distance;
+    }
+}
---
+++ Assets/Tumbleweeds/Scripts/Player/PlayerSpeedController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerSpeedController : MonoBehaviour
+{
+    [Header("Speed")]
+    public float CurrentSpeed;
+    public float MaxSpeed;
+    public float MaxHorizontalSpeed;
+    public float HorizontalSpeedScale;
+
+    [Header("Deceleration")]
+    public float DecelerationScale;
+    public AnimationCurve DecelerationCurve;
+
+    /* *** */
+
+    private Rigidbody _rigidbody;
+
+
+    private const string HORIZONTAL_AXIS = "Horizontal";
+
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = _rigidbody.velocity;
+
+        velocity.x = Mathf.Clamp(
+            value: velocity.x += Input.GetAxis(HORIZONTAL_AXIS)  * HorizontalSpeedScale * CurrentSpeed * Time.deltaTime, 
+            min: -MaxHorizontalSpeed, 
+            max: +MaxHorizontalSpeed
+        );
+
+        CurrentSpeed -= DecelerationCurve.Evaluate(Mathf.InverseLerp(0, MaxSpeed, CurrentSpeed)) * DecelerationScale;
+
+        velocity.z = CurrentSpeed;
+
+        _rigidbody.velocity = velocity;
+    }
+}
