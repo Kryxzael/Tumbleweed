04b82a422036e53b6e3ced7a6010b8f205b7cb8f
Kryxzael <kasper.ramskjell98@gmail.com>
2023-02-04 21:42:19 +0100

    Add crashable spawns to world generator

---
+++ Assets/Tumbleweeds/Scripts/WorldGen/CrashableSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+public class CrashableSpawner : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1f, 0f, 0f);
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.up);
+    }
+}
--- Assets/Tumbleweeds/Scripts/WorldGen/WorldGenerator.cs
+++ Assets/Tumbleweeds/Scripts/WorldGen/WorldGenerator.cs
@@ -12,7 +12,13 @@ public class WorldGenerator : MonoBehaviour
     private const int INITIAL_SPAWN_COUNT = 10;
     private const float NEXT_SPAWN_THRESHOLD = 50f;
 
+    private int _chunksSinceLastSpawn = 0;
+    public float MaxChunksWithoutSpawn = 3;
+    public float[] SpawnChances = Array.Empty<float>(); //Item n is the chance of n enemies spawning in a chunk
+
+
     public LevelChunk[] Chunks = Array.Empty<LevelChunk>();
+    public Crashable[] Crashables = Array.Empty<Crashable>();
 
     public int NextSpawnPosition = 0;
     private Queue<LevelChunk> _loadedChunks = new Queue<LevelChunk>();
@@ -39,6 +45,7 @@ public class WorldGenerator : MonoBehaviour
 
     public void SpawnNext(bool destroyOldestChunk)
     {
+        //Select and spawn the chunk
         LevelChunk chunk;
 
         do
@@ -46,7 +53,30 @@ public class WorldGenerator : MonoBehaviour
             chunk = Chunks[UnityEngine.Random.Range(0, Chunks.Length)];
         } while (chunk.PickChance < UnityEngine.Random.value);
 
-        _loadedChunks.Enqueue(Instantiate(chunk, new Vector3(0, 0, NextSpawnPosition * CHUNK_SIZE), Quaternion.identity));
+        LevelChunk spawned = Instantiate(chunk, new Vector3(0, 0, NextSpawnPosition * CHUNK_SIZE), Quaternion.identity);
+        _loadedChunks.Enqueue(spawned);
+
+        //Spawn crashables
+        int enemySpawnCount;
+
+        do
+        {
+            enemySpawnCount = UnityEngine.Random.Range(0, SpawnChances.Length);
+        } while (SpawnChances[enemySpawnCount] < UnityEngine.Random.value || (enemySpawnCount == 0 && _chunksSinceLastSpawn >= MaxChunksWithoutSpawn));
+
+        if (enemySpawnCount == 0)
+            _chunksSinceLastSpawn++;
+
+        else
+            _chunksSinceLastSpawn = 0;
+
+        foreach (CrashableSpawner i in spawned.GetComponentsInChildren<CrashableSpawner>().OrderBy(i => UnityEngine.Random.value).Take(enemySpawnCount))
+        {
+            Crashable crashable = Crashables[UnityEngine.Random.Range(0, Crashables.Length)];
+            Instantiate(crashable, i.transform.position, Quaternion.identity, spawned.transform);
+        }
+
+        //Cleanup
         NextSpawnPosition += chunk.SizeMultiplier;
 
         if (destroyOldestChunk)
