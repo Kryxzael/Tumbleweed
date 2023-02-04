3036d6d4bce00a7093058bd052b8b7811c3db909
Kryxzael <kasper.ramskjell98@gmail.com>
2023-02-04 23:17:16 +0100

    Add moving enemies

---
+++ Assets/Tumbleweeds/Scripts/Object/WalkingActor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+public class WalkingActor : MonoBehaviour
+{
+    public float MinSpeed;
+    public float MaxSpeed;
+    private float _speed;
+
+    public bool MovingRight;
+
+    private void Start()
+    {
+        _speed = UnityEngine.Random.Range(MinSpeed, MaxSpeed);
+        transform.eulerAngles = transform.eulerAngles.SetY(MovingRight ? 90 : 270);
+    }
+
+    private void Update()
+    {
+        transform.TranslateZ(_speed * Time.deltaTime);
+
+        if (Physics.Raycast(transform.position, transform.forward, 1f))
+        {
+            MovingRight = !MovingRight;
+            transform.eulerAngles = transform.eulerAngles.SetY(MovingRight ? 90 : 270);
+        }
+    }
+}
