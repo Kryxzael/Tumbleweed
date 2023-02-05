9cd691c3d835cc829e55c4b29c319ce49b121369
Kryxzael <kasper.ramskjell98@gmail.com>
2023-02-05 17:01:49 +0100

    Add a shadow

---
+++ Assets/Tumbleweeds/Scripts/Player/Shadow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+public class Shadow : MonoBehaviour
+{
+    public Transform Target;
+    public float Y;
+
+    private void LateUpdate()
+    {
+        transform.position = Target.position.SetY(Y);
+    }
+}
